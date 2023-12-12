90fc3c5c0c82c34dd08c4675cbd7e99fb36c1e9a
tomoaki tsuzuki <45953710+tsuzukid@users.noreply.github.com>
2023-12-12 10:27:00 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ echo "" >> ~/.bashrc
 echo "alias dp='docker ps'" >> ~/.bashrc
 echo "alias gs='git status'" >> ~/.bashrc
 echo "alias gb='git branch'" >> ~/.bashrc
+echo "alias gp='git push origin HEAD'" >> ~/.bashrc
 
 echo "export LS_OPTIONS='--color=auto'" >> ~/.bashrc
